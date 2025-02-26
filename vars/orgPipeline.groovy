df10d29428dc338e57dafb8bc8249e55509acda7
devops-cyparta <devops@cyparta.com>
2025-02-26 13:02:27 +0200

    Update orgPipeline.groovy

--- vars/orgPipeline.groovy
+++ vars/orgPipeline.groovy
@@ -3,9 +3,9 @@ def call() {
         agent any
 
         environment {
-            REPO_NAME = "${env.JOB_NAME}".split("/")[1]
+            REPO_NAME = "${env.JOB_NAME.split('/')[1]}"
             BRANCH_NAME = "${env.BRANCH_NAME}"
-            USER_NAME = sh(script: "git log --format='%an' -n 1", returnStdout: true).trim()
+            USER_NAME = "${sh(script: 'git log --format=\'%an\' -n 1', returnStdout: true).trim()}"
             BUILD_NUM = "${env.BUILD_NUMBER}"
             STORAGE_PATH = "/mnt/Storage/${env.GITHUB_ORG}/${USER_NAME}/${REPO_NAME}/${BRANCH_NAME}"
             IMAGE_NAME = "${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}:${BUILD_NUM}"
@@ -26,10 +26,7 @@ def call() {
             stage('Build Docker Image') {
                 steps {
                     script {
-                        sh """
-                        cd \"${STORAGE_PATH}\"
-                        docker build -t \"${IMAGE_NAME}\" .
-                        """
+                        sh "cd \"${STORAGE_PATH}\" && docker build -t \"${IMAGE_NAME}\" ."
                     }
                 }
             }
@@ -37,10 +34,8 @@ def call() {
             stage('Stop & Remove Old Containers') {
                 steps {
                     script {
-                        sh """
-                        docker ps -a | grep \"${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}\" | awk '{print \$1}' | xargs -r docker stop
-                        docker ps -a | grep \"${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}\" | awk '{print \$1}' | xargs -r docker rm
-                        """
+                        sh "docker ps -a | grep \"${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}\" | awk '{print \$1}' | xargs -r docker stop"
+                        sh "docker ps -a | grep \"${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}\" | awk '{print \$1}' | xargs -r docker rm"
                     }
                 }
             }
@@ -50,18 +45,12 @@ def call() {
                     script {
                         def port = sh(script: "shuf -i 2000-65000 -n 1", returnStdout: true).trim()
                         try {
-                            // Save the last successful image before replacing it
                             sh "docker tag \"${IMAGE_NAME}\" latest-${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}"
-
-                            // Run new container
-                            sh """
-                            docker run -d -p \"${port}:80\" --name \"${IMAGE_NAME}\" \"${IMAGE_NAME}\"
-                            echo \"Running on port ${port}\"
-                            """
+                            sh "docker run -d -p \"${port}:80\" --name \"${IMAGE_NAME}\" \"${IMAGE_NAME}\""
+                            echo "Running on port ${port}"
                         } catch (Exception e) {
-                            // If failed, rollback to last working version
                             echo "Deployment failed, rolling back to previous version..."
-                            sh "docker run -d -p \"${port}:80\" --name rollback-${IMAGE_NAME} latest-${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}"
+                            sh "docker run -d -p \"${port}:80\" --name \"rollback-${IMAGE_NAME}\" latest-${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}"
                         }
                     }
                 }
@@ -70,9 +59,7 @@ def call() {
             stage('Cleanup Old Images') {
                 steps {
                     script {
-                        sh """
-                        docker images | grep \"${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}\" | awk '{print \$3}' | tail -n +3 | xargs -r docker rmi -f
-                        """
+                        sh "docker images | grep \"${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}\" | awk '{print \$3}' | tail -n +3 | xargs -r docker rmi -f"
                     }
                 }
             }
@@ -80,9 +67,7 @@ def call() {
             stage('Cleanup Old Builds') {
                 steps {
                     script {
-                        sh """
-                        ls -dt \"${STORAGE_PATH}/../*/\" | tail -n +6 | xargs -r rm -rf
-                        """
+                        sh "ls -dt \"${STORAGE_PATH}/../*/\" | tail -n +6 | xargs -r rm -rf"
                     }
                 }
             }
@@ -97,3 +82,4 @@ def call() {
         }
     }
 }
+
