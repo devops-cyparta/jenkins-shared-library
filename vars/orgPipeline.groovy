d936a8889dd679b802c70d8bb9de68bb2e2fb993
devops-cyparta <devops@cyparta.com>
2025-02-26 12:53:16 +0200

    Update orgPipeline.groovy

--- vars/orgPipeline.groovy
+++ vars/orgPipeline.groovy
@@ -1,13 +1,12 @@
-// vars/orgPipeline.groovy
 def call() {
     pipeline {
         agent any
 
         environment {
-            REPO_NAME = env.JOB_NAME.split("/")[1]
-            BRANCH_NAME = env.BRANCH_NAME
+            REPO_NAME = "${env.JOB_NAME}".split("/")[1]
+            BRANCH_NAME = "${env.BRANCH_NAME}"
             USER_NAME = sh(script: "git log --format='%an' -n 1", returnStdout: true).trim()
-            BUILD_NUM = env.BUILD_NUMBER
+            BUILD_NUM = "${env.BUILD_NUMBER}"
             STORAGE_PATH = "/mnt/Storage/${env.GITHUB_ORG}/${USER_NAME}/${REPO_NAME}/${BRANCH_NAME}"
             IMAGE_NAME = "${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}:${BUILD_NUM}"
         }
@@ -16,10 +15,10 @@ def call() {
             stage('Checkout') {
                 steps {
                     script {
-                        sh "mkdir -p ${STORAGE_PATH}"
-                        sh "rm -rf ${STORAGE_PATH}/*"
+                        sh "mkdir -p \"${STORAGE_PATH}\""
+                        sh "rm -rf \"${STORAGE_PATH}/*\""
                         checkout scm
-                        sh "cp -r * ${STORAGE_PATH}/"
+                        sh "cp -r * \"${STORAGE_PATH}/\""
                     }
                 }
             }
@@ -28,8 +27,8 @@ def call() {
                 steps {
                     script {
                         sh """
-                        cd ${STORAGE_PATH}
-                        docker build -t ${IMAGE_NAME} .
+                        cd \"${STORAGE_PATH}\"
+                        docker build -t \"${IMAGE_NAME}\" .
                         """
                     }
                 }
@@ -39,8 +38,8 @@ def call() {
                 steps {
                     script {
                         sh """
-                        docker ps -a | grep '${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}' | awk '{print \$1}' | xargs -r docker stop
-                        docker ps -a | grep '${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}' | awk '{print \$1}' | xargs -r docker rm
+                        docker ps -a | grep \"${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}\" | awk '{print \$1}' | xargs -r docker stop
+                        docker ps -a | grep \"${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}\" | awk '{print \$1}' | xargs -r docker rm
                         """
                     }
                 }
@@ -52,28 +51,27 @@ def call() {
                         def port = sh(script: "shuf -i 2000-65000 -n 1", returnStdout: true).trim()
                         try {
                             // Save the last successful image before replacing it
-                            sh "docker tag ${IMAGE_NAME} latest-${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}"
-            
+                            sh "docker tag \"${IMAGE_NAME}\" latest-${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}"
+
                             // Run new container
                             sh """
-                            docker run -d -p ${port}:80 --name ${IMAGE_NAME} ${IMAGE_NAME}
-                            echo "Running on port ${port}"
+                            docker run -d -p \"${port}:80\" --name \"${IMAGE_NAME}\" \"${IMAGE_NAME}\"
+                            echo \"Running on port ${port}\"
                             """
                         } catch (Exception e) {
                             // If failed, rollback to last working version
                             echo "Deployment failed, rolling back to previous version..."
-                            sh "docker run -d -p ${port}:80 --name rollback-${IMAGE_NAME} latest-${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}"
+                            sh "docker run -d -p \"${port}:80\" --name rollback-${IMAGE_NAME} latest-${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}"
                         }
                     }
                 }
             }
 
-
             stage('Cleanup Old Images') {
                 steps {
                     script {
                         sh """
-                        docker images | grep '${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}' | awk '{print \$3}' | tail -n +3 | xargs -r docker rmi -f
+                        docker images | grep \"${BRANCH_NAME}-${REPO_NAME}-${USER_NAME}\" | awk '{print \$3}' | tail -n +3 | xargs -r docker rmi -f
                         """
                     }
                 }
@@ -83,7 +81,7 @@ def call() {
                 steps {
                     script {
                         sh """
-                        ls -dt ${STORAGE_PATH}/../*/ | tail -n +6 | xargs -r rm -rf
+                        ls -dt \"${STORAGE_PATH}/../*/\" | tail -n +6 | xargs -r rm -rf
                         """
                     }
                 }
